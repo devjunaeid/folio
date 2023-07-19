b1c3c7e7fd1408f705ee0f10cb4139f2167e1924
Junaeid <junaeid.md48@gmail.com>
2023-07-19 19:42:38 +0600

    Added About me section with new intro layout and colors

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const hamMenu = document.querySelector("#hamMenu");
 const menu = document.querySelector("#dropMenu");
-
-
 hamMenu.addEventListener("click", ()=>{
     menu.classList.toggle("hidden");
 })
