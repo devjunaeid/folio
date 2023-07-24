d7fffc2917845275b8edc3221e831e45fa74d77f
Junaeid <junaeid.md48@gmail.com>
2023-07-24 13:13:50 +0600

    Adding Functionality for hamburger menu

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const hamMenu = document.querySelector("#hamMenu");
 const menu = document.querySelector("#dropMenu");
+const links = document.querySelectorAll("li")
 hamMenu.addEventListener("click", ()=>{
     menu.classList.toggle("hidden");
 })
